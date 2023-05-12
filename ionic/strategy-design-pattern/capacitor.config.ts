6048ffc0439d02405cbcc793136e8e6266a2f47d
nativacu <nativacu@gmail.com>
2023-05-12 14:14:45 -0400

    initial commit

---
+++ ionic/strategy-design-pattern/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'strategy-design-pattern',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
